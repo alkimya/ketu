1b9987eedaee9461adbae0d1e7e8338e8450e70f
Loc Cosnier <loc.cosnier@pm.me>
2021-02-08 13:16:06 +0100

    Test: Update test_ketu.py

--- tests/test_ketu.py
+++ tests/test_ketu.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 import numpy as np
 
-from ketu.ketu import (bodies, signs, dd_to_dms, distance, get_orb,
+from ketu.ketu import (bodies, aspects, signs, dd_to_dms, distance, get_orb,
                        local_to_utc, utc_to_julian, body_name, body_properties,
                        body_id, body_long, body_lat, body_distance, body_vlong,
                        body_vlat, body_vdistance, is_retrograde, is_ascending,
@@ -13,6 +13,18 @@ zero_day = utc_to_julian(-4713, 11, 24, 12, 0, 0)
 
 
 class KetuTest(TestCase):
+
+    def test_bodies(self):
+        self.assertEqual(len(bodies), 11)
+        self.assertEqual(bodies['id'][0], 0)
+
+    def test_aspects(self):
+        self.assertEqual(len(aspects), 5)
+        self.assertEqual(aspects['value'][0], 0)
+
+    def test_signs(self):
+        self.assertEqual(len(signs), 12)
+        self.assertEqual(signs[2], 'Gemini')
 
     def test_local_to_utc(self):
         self.assertAlmostEqual(local_to_utc(2020, 12, 21, 19, 20, 0, 1)[-1]
@@ -26,11 +38,10 @@ class KetuTest(TestCase):
 
     def test_distance(self):
         # Test reflexivity of distance
-        self.assertEqual(
-            distance(body_long(jday, 0), body_long(jday, 1)),
-            distance(body_long(jday, 1), body_long(jday, 0)))
-        self.assertAlmostEqual(distance(body_long(jday, 0),
-                                        body_long(jday, 1)), 90, delta=3)
+        dist, long = distance, body_long
+        self.assertEqual(dist(long(jday, 0), long(jday, 1)),
+                         dist(long(jday, 1), long(jday, 0)))
+        self.assertAlmostEqual(dist(long(jday, 0), long(jday, 1)), 90, delta=3)
 
     def test_get_orb(self):
         self.assertAlmostEqual(get_orb(0, 1, 3), 8, delta=0.001)
@@ -73,6 +84,10 @@ class KetuTest(TestCase):
     def test_body_sign(self):
         self.assertEqual(signs[body_sign(body_long(jday, 0))[0]], 'Capricorn')
 
+    def test_positions(self):
+        sign = body_sign(positions(jday, bodies)[0])[0]
+        self.assertEqual(signs[sign], 'Capricorn')
+
     def test_get_aspect(self):
         self.assertEqual(get_aspect(jday, 5, 6)[2], 0)
         self.assertAlmostEqual(get_aspect(jday, 5, 6)[3], 0, delta=0.1)
@@ -81,12 +96,10 @@ class KetuTest(TestCase):
         asps = get_aspects(jday)
         asps2 = asps[np.where(asps['body1'] == 5)]
         body1, body2, aspect, orb = asps2[np.where(asps2['body2'] == 6)][0]
+        self.assertEqual(body1, 5)
+        self.assertEqual(body2, 6)
         self.assertEqual(aspect, 0)
         self.assertAlmostEqual(orb, 0, delta=1)
 
     def test_is_applicative(self):
         pass  # in dev mode
-
-    def test_bodies_positions(self):
-        sign = body_sign(positions(jday, bodies)[0])[0]
-        self.assertEqual(signs[sign], 'Capricorn')
