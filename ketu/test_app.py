bf4c71eaa26d932fa0996266678749dbb00d13fa
Loc Cosnier <loc.cosnier@pm.me>
2023-11-22 23:44:54 +0100

    Refactor with new profiling module

--- ketu/test_app.py
+++ ketu/test_app.py
@@ -7,20 +7,21 @@ from numpy import fromiter
 import swisseph as swe
 
 from ketu import (
-    utc_to_julian, bodies, maspects, properties, print_aspects, print_positions, dms_to_dd,
-    get_aspect_orb, julian_to_utc, find_easpect, get_aspects, get_chart, get_angle, get_angles,
+    utc_to_julian, utc_to_julian2, bodies, bodies_name, aspects, get_bodies, properties, print_aspects, print_positions, dms_to_dd,
+    get_aspect_orb, julian_to_local, find_easpect, find_easpect2, get_aspects, get_chart, get_angle, get_angles,
     gen_aspects, get_aspect
 )
-from profile import timing
+from timea import timea
 
 zoneinfo = ZoneInfo('Europe/Paris')
-gday = datetime(1, 1, 1, 12, 0, 0, tzinfo=zoneinfo)
-jday = utc_to_julian(gday, zoneinfo)
+gday = datetime.now()
+jday = utc_to_julian(gday)
+jday2 = utc_to_julian2(gday)
 lemans = 48.0042, 0.1970, 100
 swe.set_topo(*lemans)
 
 
-@timing
+@timea
 def print_array(array):
     """Print the characteristics of an array"""
     print(f"array : {array}")
@@ -46,9 +47,14 @@ def main():
     print_aspects(jday)
     print("\n")
     print(f"Initial datetime : \n{gday:}")
-    print(f"Datetime for next aspect Sun-Moon : \n{julian_to_utc(find_easpect(get_aspect(chart, 0, 1)), zoneinfo)}")
-    print(f"Datetime for moon rise : \n{julian_to_utc(swe.rise_trans(jday, 1, 1, *lemans)[1][0], zoneinfo)}")
+    print(f"Datetime for next aspect Sun-Moon recursive: \n{julian_to_utc(find_easpect(get_aspect(chart, 0, 1)), zoneinfo)}")
+#   print(f"Datetime for moon rise : \n{julian_to_utc(swe.rise_trans(jday, 1, 1, *lemans)[1][0], zoneinfo)}")
+    print(f"Datetime for next aspect Sun-Moon iterative: \n{julian_to_utc(find_easpect2(get_aspect(chart, 0, 1)), zoneinfo)}")
+#  print(f"Datetime for moon rise : \n{julian_to_utc(swe.rise_trans(jday, 1, 1, *lemans)[1][0], zoneinfo)}")
 
 
 if __name__ == "__main__":
-    main()
+    #main()
+    print(bodies_name(get_bodies([1, 10, 12])))
+    print(jday)
+    print(jday2)
